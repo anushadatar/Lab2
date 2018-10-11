29f08c403181572425722a8720cc48885ededfa1
anushadatar <anushadatar@gmail.com>
2018-10-11 18:28:20 -0400

    Added shift register comment

--- shiftregister.t.v
+++ shiftregister.t.v
@@ -25,17 +25,28 @@ module testshiftregister();
     always #30 peripheralClkEdge=!peripheralClkEdge;
     initial parallelLoad = 0;
 
+    /*
+    This test runs both modes of the shift register : Parallel In, serial Out
+    and Serial In, Parallel Out. Use the run_test script for the shift register
+    to view the waveforms and check that the data has loaded as expected 
+    (shifted bit by bit for serial in with a full output for the corresponding
+    paralleut and a full replacement for parallelin with the MSB reported for
+    serialout). 
+    */
+
     initial begin
       $dumpfile("shiftregister.vcd");
       $dumpvars(0, dut);
 
       serialDataIn = 1;
       #500
-
       parallelDataIn = 8'hAA;
 
+      // Parallel In, Serial Out
       #100
       parallelLoad = 1;
+      
+      // Serial In, Parallel Out
       #100
       parallelLoad = 0;
 
