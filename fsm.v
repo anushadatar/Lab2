a9570d852ac542cb022f0ccbdc07a0a337089926
Lauren Anfenson <laurenanfenson@gmail.com>
2018-10-17 14:17:33 -0400

    address is being read and miso enables, need to add d flip flop to get her to talk to us

--- fsm.v
+++ fsm.v
@@ -12,30 +12,37 @@ module fsm(
   reg[3:0] counter = 4'd15;
 
   always @ (posedge sclk_pin) begin
+    if (!cs_pin) begin
     counter <= counter + 1;
     case(counter)
       4'd0: begin
         addr_wr <= 1'b1; s_r <= 1'b0; dm_wr <= 1'b0; miso_en <= 1'b0;
       end
-      4'd7: begin
+      4'd6: begin
         addr_wr <= 1'b0;
         if(!r_or_w) begin
-          s_r <= 1'b1; dm_wr <= 1'b0; miso_en <= 1'b1;
-        end
-        else begin
           s_r <= 1'b0; dm_wr <= 1'b1; miso_en <= 1'b0;
         end
+        else begin
+          s_r <= 1'b1; dm_wr <= 1'b0; miso_en <= 1'b1;
+        end
       end
-      4'd8: begin
-        s_r <= 1'b0;
-      end
+      //4'd8: begin
+        //s_r <= 1'b0;
+      //end
       4'd15: begin
-        addr_wr <= 1'b0; s_r <= 1'b0; dm_wr <= 1'b0; miso_en <= miso_en;
+        addr_wr <= 1'b0; s_r <= 1'b0; dm_wr <= 1'b0; miso_en <= 1'b0;
       end
       default: begin
         addr_wr <= addr_wr; s_r <= s_r; dm_wr <= dm_wr; miso_en <= miso_en;
       end
     endcase
+    end
+    if(cs_pin) begin
+      counter <= 4'd15;
+      addr_wr <= 1'b0; s_r <= 1'b0; dm_wr <= 1'b0; miso_en <= 1'b0;
+    end
+
   end
 
 endmodule
--- spimemory.t.v
+++ spimemory.t.v
@@ -14,23 +14,25 @@ module spiMemoryTest();
   initial clk =0;
   always #5 clk = !clk;
 
+  initial sclk =0;
+  always #200 sclk = !sclk;
+
   initial begin
       $dumpfile("spitest.vcd");
       $dumpvars();
-      clk=0;sclk=0;cs_pin=1;mosi_pin=0;
-      #500
+      cs_pin=1;mosi_pin=0;
+      #800
 
-      cs_pin = 0;
+      //cs_pin = 0;
 
       // Give the 6 address from MSB to LSB
       for (i=0; i<7; i=i+1) begin
-          mosi_pin = WRITE_ADDRESS[6-i]; #200
-          sclk=1;#200
-          sclk=0;
+          #400
+          cs_pin = 0;
+          mosi_pin = WRITE_ADDRESS[6-i];
       end
-      mosi_pin = 0; #100 // Specify the operation to be write
-      sclk=1;#200
-      sclk = 0;
+      #400
+      mosi_pin = 0; // Specify the operation to be write
 
       if (dut.memory.address != READ_ADDRESS) begin
         $display("Test failed. Address incorrect.");
@@ -39,35 +41,38 @@ module spiMemoryTest();
 
       // Transmit the data to be written from MSB to LSB
       for (i=0; i<8; i=i+1) begin
-          mosi_pin = DATA[7-i]; #200
-          sclk=1; #200
-          sclk = 0;
+          #400
+          mosi_pin = DATA[7-i];
       end
+
       #100
       cs_pin = 1;
-
-
-      #100
-      cs_pin = 0;
+      #400
 
       // Give the 6 address from MSB to LSB
       for (i=0; i<7; i=i+1) begin
-          mosi_pin = READ_ADDRESS[6-i];#200
-          sclk=1;#200
-          sclk=0;
+          #400
+          cs_pin = 0;
+          mosi_pin = READ_ADDRESS[6-i];
       end
-      mosi_pin = 0; #100 // Specify the operation to be write
-      sclk=1;#200
-      sclk = 0;
+      #400
+      mosi_pin = 1; #400 // Specify the operation to be read
+      mosi_pin = 0;
+
       // Transmit the data to be written from MSB to LSB
-      for (i=0; i < 8; i=i+1) begin
-          mosi_pin = DATA[0]; #200
+      //for (i=0; i < 8; i=i+1) begin
+          //mosi_pin = DATA[0]; #200
           // if (miso_pin != DATA[7-i]) begin #100
           //    $display("Test failed at bit i");
           // end
-          sclk=1;#200
-          sclk=0;
-      end
-      $finish;
+          //sclk=1;#200
+          //sclk=0;
+          //for (i=0; i<20; i=i+1) begin
+            //sclk = 1; #200
+            //sclk = 0;
+          //end
+      //#2000
+      //end
+      //$finish;
   end
 endmodule
