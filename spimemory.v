ede47740ee69cfddf424e333e517a04a9b553f15
anushadatar <anushadatar@gmail.com>
2018-10-18 18:42:15 -0400

    We pass the data test and have a DFF. I guess you could say BIG TIME PLAYERS!

--- spimemory.v
+++ spimemory.v
@@ -41,7 +41,7 @@ module spiMemory
                          .parallelDataOut(Din),
                          .serialDataOut(serialDataOut));
 
-  // dff dff(.serialDataOut(serialDataOut), .clockEnable(sclk_f), .clk(clk), .dataOut(dataOut));
+  dff dff(.serialDataOut(serialDataOut), .clockEnable(sclk_f), .clk(clk), .dataOut(dataOut));
 
   assign miso_pin = (miso_en) ? serialDataOut : 1'bz;
 
